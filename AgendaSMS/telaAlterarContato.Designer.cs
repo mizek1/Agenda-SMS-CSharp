eb157ca13b6541002e6741caa4738e96f9a85fae
Danilo Alves <daniloalves@aluno.fapce.edu.br>
2020-05-13 12:27:13 -0300

    tela Alterar Contato

---
+++ AgendaSMS/telaAlterarContato.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace AgendaSMS
+{
+    partial class telaAlterarContato
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(telaAlterarContato));
+            this.txtAniversario = new System.Windows.Forms.TextBox();
+            this.txtTelefone = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.btnDescartar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtAniversario
+            // 
+            this.txtAniversario.BackColor = System.Drawing.Color.White;
+            this.txtAniversario.Location = new System.Drawing.Point(17, 173);
+            this.txtAniversario.Name = "txtAniversario";
+            this.txtAniversario.Size = new System.Drawing.Size(491, 20);
+            this.txtAniversario.TabIndex = 16;
+            this.txtAniversario.Text = "Aniversário";
+            // 
+            // txtTelefone
+            // 
+            this.txtTelefone.BackColor = System.Drawing.Color.White;
+            this.txtTelefone.Location = new System.Drawing.Point(17, 135);
+            this.txtTelefone.Name = "txtTelefone";
+            this.txtTelefone.Size = new System.Drawing.Size(491, 20);
+            this.txtTelefone.TabIndex = 15;
+            this.txtTelefone.Text = "Telefone";
+            // 
+            // txtNome
+            // 
+            this.txtNome.BackColor = System.Drawing.Color.White;
+            this.txtNome.Location = new System.Drawing.Point(17, 97);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(491, 20);
+            this.txtNome.TabIndex = 14;
+            this.txtNome.Text = "Nome";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(192, 32);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Alterar Contato";
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Image = ((System.Drawing.Image)(resources.GetObject("btnSalvar.Image")));
+            this.btnSalvar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSalvar.Location = new System.Drawing.Point(350, 221);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(65, 23);
+            this.btnSalvar.TabIndex = 18;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            // 
+            // btnDescartar
+            // 
+            this.btnDescartar.Image = ((System.Drawing.Image)(resources.GetObject("btnDescartar.Image")));
+            this.btnDescartar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnDescartar.Location = new System.Drawing.Point(430, 221);
+            this.btnDescartar.Name = "btnDescartar";
+            this.btnDescartar.Size = new System.Drawing.Size(79, 23);
+            this.btnDescartar.TabIndex = 17;
+            this.btnDescartar.Text = "Descartar";
+            this.btnDescartar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnDescartar.UseVisualStyleBackColor = true;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(521, 278);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.btnDescartar);
+            this.Controls.Add(this.txtAniversario);
+            this.Controls.Add(this.txtTelefone);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label1);
+            this.Name = "Form2";
+            this.Text = "Alterar contato";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.Button btnDescartar;
+        private System.Windows.Forms.TextBox txtAniversario;
+        private System.Windows.Forms.TextBox txtTelefone;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label label1;
+    }
+}
