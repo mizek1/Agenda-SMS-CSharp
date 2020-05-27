b9ba814b3e18c24b9c46b4f3a05391b9bec2a50b
Danilo Alves <daniloalves@aluno.fapce.edu.br>
2020-05-27 15:21:13 -0300

    conectar usando odbc

--- AgendaSMS/Program.cs
+++ AgendaSMS/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data;
+using System.Data.Odbc;
 
 namespace AgendaSMS
 {
@@ -14,6 +16,48 @@ namespace AgendaSMS
         [STAThread]
         static void Main()
         {
+            String szConnect =  "DSN=dsnname;" +
+                                "UID=postgres" +
+                                "PWD=*******";
+
+            OdbcConnection cnDB = new OdbcConnection(szConnect);
+
+            try
+            {
+                cnDB.Open();
+            } catch (OdbcException ex)
+            {
+                Console.WriteLine(ex.Message + "\n\n" + "StackTrace: \n\n" + ex.StackTrace);
+                Console.WriteLine("\nPress  to continue...");
+                Console.Read();
+                return;
+            }
+
+            DataSet dsDB = new DataSet();
+            OdbcDataAdapter adDB = new OdbcDataAdapter();
+            OdbcCommandBuilder cbDB = new OdbcCommandBuilder(adDB);
+            adDB.SelectCommand = new OdbcCommand(
+                                     "SELECT * FROM contato",
+                                     cnDB);
+            adDB.Fill(dsDB);
+
+            // Display the record count
+            Console.WriteLine("Table 'csharptest' contains {0} rows.\n",
+                              dsDB.Tables[0].Rows.Count);
+
+            // List the columns (using a foreach loop)
+            Console.WriteLine("Columns\n=======\n");
+
+            foreach (DataColumn dcDB in dsDB.Tables[0].Columns)
+                Console.WriteLine("{0} ({1})", dcDB.ColumnName, dcDB.DataType);
+            Console.WriteLine("\n");
+
+            // Iterate through the rows and display the data in the table (using a for loop).
+            // Display the data column last for readability.
+            Console.WriteLine("Data\n====\n");
+            //for (int i = 0; i to continue...");
+            Console.Read();
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form_Usuario());
